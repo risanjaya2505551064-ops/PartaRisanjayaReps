251e3ee96b874519e4da15f493c4a8203f47ceb5
Parta <[200~risanjaya.2505551064@student.unud.ac.id>
2025-10-09 00:22:20 +0800

    C1

---
+++ KALKULATOR.c
@@ -0,0 +1,174 @@
+#include <stdio.h>
+
+void starting() {
+    int i;
+    int stip = 80;
+    for (i = 0; i < stip; i++) {
+        printf("*");
+    }
+    printf("\n======================= WELCOME TO KALKULATOR KELOMPOK 10 ======================\n");
+    for (i = 0; i < stip; i++) {
+        printf("*");
+    }
+}
+
+void displayMenu() {
+    int e;
+    for (e = 0; e < 2; e++) {
+        printf("\n");
+    }
+    printf("\n= SILAHKAN PILIH OPERASI YANG INGIN DIJALANKAN =\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("5. Modulation\n");
+    printf("6. Exit\n");
+    printf("Pilih Operasi Yang diinginkan (1-6): ");
+}
+
+void getTwoNumbers(float* num1, float* num2) {
+    printf("Masukkan Angka pertama  = ");
+    while (scanf("%f", num1) != 1) {
+        printf("== Invalid input. Please enter a number only ==.\n");
+        while (getchar() != '\n');
+        printf("Masukkan Angka pertama  = ");
+    }
+
+    printf("Masukkan Angka kedua    = ");
+    while (scanf("%f", num2) != 1) {
+        printf("== Invalid input. Please enter a number only ==.\n");
+        while (getchar() != '\n');
+        printf("Masukkan Angka kedua    = ");
+    }
+}
+
+void addition() {
+    float num1, num2, result;
+    getTwoNumbers(&num1, &num2);
+    result = num1 + num2;
+    printf("Result= %.2f\n", result);
+}
+
+void subtraction() {
+    float num1, num2, result;
+    getTwoNumbers(&num1, &num2);
+    result = num1 - num2;
+    printf("Result= %.2f\n", result);
+}
+
+void multiplication() {
+    float num1, num2, result;
+    getTwoNumbers(&num1, &num2);
+    result = num1 * num2;
+    printf("Result= %.2f\n", result);
+}
+
+void division() {
+    float num1, num2, result;
+    getTwoNumbers(&num1, &num2);
+    if (num2 != 0) {
+        result = num1 / num2;
+        printf("Result= %.2f\n", result);
+    } else {
+        printf("== Error Ngabs! Pembagian Ga bisa Pake 0 ==\n");
+    }
+}
+
+void modulo() {
+    int num1, num2, result;
+
+    printf("Masukkan Angka pertama  = ");
+    while (scanf("%d", &num1) != 1) {
+        printf("== Error Ngabs! Tolong Input Integer Only ==.\n");
+        while (getchar() != '\n');
+        printf("Masukkan Angka pertama  = ");
+    }
+
+    printf("Masukkan Angka kedua  = ");
+    while (scanf("%d", &num2) != 1) {
+        printf("== Error Ngabs! Tolong Input Integer Only ==.\n");
+        while (getchar() != '\n');
+        printf("Masukkan Angka kedua  = ");
+    }
+
+    if (num2 != 0) {
+        result = num1 % num2;
+        printf("Result= %d\n", result);
+    } else {
+        printf("== Error Ngabs! Modulo Ga bisa Pake 0 ==\n");
+    }
+}
+
+int main() {
+    int choice;
+    char cont;
+    starting();
+    do {
+        displayMenu();
+
+        while (scanf("%d", &choice) != 1) {
+            printf("\n\n\n== Error Ngabs! Tolong Masukkan angka antara 1 - 6 ==\n");
+            while (getchar() != '\n');
+            displayMenu();
+        }
+
+        switch (choice) {
+            case 1:
+                addition();
+                break;
+            case 2:
+                subtraction();
+                break;
+            case 3:
+                multiplication();
+                break;
+            case 4:
+                division();
+                break;
+            case 5:
+                modulo();
+                break;
+            case 6:
+                for (int i = 0; i < 80; i++) {
+                   printf("*");
+                }
+                printf("\n              == TERIMAKASIH TELAH MENGGUNAKAN KALKULATOR INI. Adieu! ==\n");
+                for (int i = 0; i < 80; i++) {
+                   printf("*");
+                }
+                continue;
+            default:
+                printf("\n\n\n          == Error Ngabs! Tolong masukkan angka antara 1 - 6 ==\n");
+                while (getchar() != '\n');
+                continue;
+        }
+
+        while (1) {
+            printf("APAKAH KAMU INGIN MELAKUKAN OPERASI LAGI? (y/n): ");
+            scanf(" %c", &cont);
+
+            while (getchar() != '\n');
+
+            if (cont == 'y' || cont == 'Y' || cont == 'n' || cont == 'N') {
+                break;
+            } else {
+                printf("== Error Ngabs! Tolong input 'y' / 'n' ==\n");
+            }
+        }
+
+        if (cont == 'n' || cont == 'N') {
+            choice = 6;
+            for (int i = 0; i < 80; i++) {
+                   printf("*");
+                }
+                printf("\n          == TERIMAKASIH TELAH MENGGUNAKAN KALKULATOR INI. Adieu! ==\n");
+                for (int i = 0; i < 80; i++) {
+                   printf("*");
+                }
+        }
+
+    } while (choice != 6);
+
+    return 0;
+}
